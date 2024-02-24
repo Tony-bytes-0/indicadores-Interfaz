de6a94cd7cb0bc9e3d96a5521d79198be1d60a16
Tony <lonson574gmail.com>
2024-02-24 06:00:42 -0400

    mantenedor medico en progreso

---
+++ src/app/register/DiseaseModal.jsx
@@ -0,0 +1,72 @@
+"use client";
+import { Box, Button, FormControl, Grid, TextField } from "@mui/material";
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import axios from "axios";
+
+export default function DiseaseModal() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  const [nombreEnfermedad, setNombreEnfermedad] = useState("");
+  const handleName = (event) => {
+    setNombreEnfermedad(event.target.value);
+  }; //NAME
+  const postNombreEnfermedad = () => {
+    axios
+      .post("http://localhost:4000/enfermedades", {
+        nombreEnfermedad: nombreEnfermedad,
+      })
+      .then((response) => {
+        alert("la enfermedad introducida ha sido creada con exito");
+        console.log({ nombreEnfermedad: nombreEnfermedad });
+        setNombreEnfermedad("");
+        setIsModalOpen(false);
+      })
+      .catch((e) => {
+        alert("error desconocido");
+      });
+  };
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        className="bg-blue-500 items-center justify-center lowercase"
+        onClick={toggleModal}
+      >
+        agregar enfermedad
+      </Button>
+      {isModalOpen ? (
+        <></>
+      ) : (
+        <Box className="bg-slate-200 fixed inset-56 flex items-center justify-center z-50 rounded">
+          <Button
+            variant="contained"
+            className="bg-red-500 top-0 right-0 absolute"
+            onClick={toggleModal}
+          >
+            X
+          </Button>
+          <Box className="w-full justify-center items-center flex">
+            <FormControl>
+              <TextField
+                label="nombre de la enfermedad"
+                onchange={handleName}
+                value={nombreEnfermedad}
+              ></TextField>
+              <Button
+                variant="contained"
+                className="bg-blue-500"
+                onClick={postNombreEnfermedad}
+              >
+                <AddIcon />
+              </Button>
+            </FormControl>
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+}
--- src/app/register/DiseaseSelect.jsx
+++ src/app/register/DiseaseSelect.jsx
@@ -1,36 +1,60 @@
+import {
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Modal,
+  Select,
+} from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setEnfermedad } from "@/redux/register/preMedicalRecord/enfermedad";
+import DiseaseModal  from "./DiseaseModal"
 
-
-import { FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material'
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setEnfermedad } from "@/redux/register/preMedicalRecord/enfermedad"
-
-import axios from 'axios'
-import { useEffect } from 'react'
+import axios from "axios";
+import { useEffect } from "react";
 
 function DiseaseSelect() {
-    const dispatch = useDispatch()
-    const [list, setList] = useState([])
+  const dispatch = useDispatch();
+  const [list, setList] = useState([]);
 
-    const selected = useSelector( state => state.enfermedades )
-    const handleEnfermedad = (event) => {dispatch(setEnfermedad(event.target.value)) }
+  const selected = useSelector((state) => state.enfermedades);
+  const handleEnfermedad = (event) => {
+    dispatch(setEnfermedad(event.target.value));
+  };
 
-    useEffect( () => {
-        axios.get('http://localhost:4000/enfermedades')
-        .then(function(response){
-            setList(response.data)
-        })
-    }, [dispatch])
+
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/enfermedades").then(function (response) {
+      setList(response.data);
+    });
+  }, [dispatch]);
 
   return (
-        <Grid item xs={12} className='w-max m-2 p-2'>
-            <FormControl fullWidth> <InputLabel>Enfermedad detectada</InputLabel>
-                <Select variant="filled" label="Enfermedad detectada" value={selected} onChange={handleEnfermedad} defaultValue = "">
-                    {list.map((e) => <MenuItem value={e.id} key={e.id + ' Enfermedad detectada'}>{e.nombreEnfermedad}</MenuItem> )}
-                </Select>
-            </FormControl>
-        </Grid>
-  )
+    <Grid item xs={12} className="w-max m-2 p-2">
+      <FormControl fullWidth>
+        {" "}
+        <InputLabel>Enfermedad detectada</InputLabel>
+        <Select
+          variant="filled"
+          label="Enfermedad detectada"
+          value={selected}
+          onChange={handleEnfermedad}
+          defaultValue=""
+        >
+          {list.map((e) => (
+            <MenuItem value={e.id} key={e.id + " Enfermedad detectada"}>
+              {e.nombreEnfermedad}
+            </MenuItem>
+          ))}
+        </Select>
+
+      </FormControl>
+      <DiseaseModal />
+    </Grid>
+  );
 }
 
-export default DiseaseSelect
+export default DiseaseSelect;
